0dbe6ac5884cfead646ad7f454ba8c344e47aac8
Rohan More <rohan110620@gmail.com>
2026-02-28 19:48:27 +0530

    changes made

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,10 +2,14 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class DemoApplication {
 
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DemoApplication.class); // 2. Add this method
+	}
 	public static void main(String[] args) {
 		System.out.println("Hello from spring boot server"); SpringApplication.run(DemoApplication.class, args);
 	}
